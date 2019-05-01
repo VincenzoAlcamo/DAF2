ebcf9b6a0b6c624811fbd2ad19869ec410fb5e7c
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2019-05-01 14:50:47 +0200

    Neighbors: added gift value column

--- gui/tabs/neighbors/neighbors.js
+++ gui/tabs/neighbors/neighbors.js
@@ -9,7 +9,7 @@ export default {
     requires: ['gifts', 'materials', 'decorations', 'usables', 'windmills']
 };
 
-var tab, container, selectShow, searchInput, smartTable, searchHandler, palRows, trGifts;
+let tab, container, selectShow, selectDays, searchInput, smartTable, searchHandler, palRows, palGifts, trGifts, giftValues, lastGiftDays, giftCache;
 
 function init() {
     tab = this;
@@ -17,11 +17,20 @@ function init() {
 
     selectShow = container.querySelector('[name=show]');
     selectShow.addEventListener('change', refresh);
-    for (var days of [7, 14, 21, 28]) {
-        var option = document.createElement('option');
-        option.value = days;
+    for (let days of [7, 14, 21, 28, 35, 42]) {
+        let option = document.createElement('option');
+        option.value = 'nogift' + days;
         option.innerText = Locale.getMessage('neighbors_nogift', days);
         selectShow.appendChild(option);
+    }
+
+    selectDays = container.querySelector('[name=days]');
+    selectDays.addEventListener('change', refresh);
+    for (let days = 7; days <= 50; days++) {
+        let option = document.createElement('option');
+        option.value = days;
+        option.innerText = Locale.getMessage('neighbors_days', days);
+        selectDays.appendChild(option);
     }
 
     searchInput = container.querySelector('[name=search]');
@@ -29,7 +38,7 @@ function init() {
 
     trGifts = document.createElement('tr');
     trGifts.className = 'giftrow';
-    trGifts.innerHTML = HtmlBr `<td colspan="9"><div>${Locale.getMessage('neighbors_giftinfo')}</div><div class="giftlist slick-scrollbar"></div></td>`;
+    trGifts.innerHTML = HtmlBr `<td colspan="10"><div>${Locale.getMessage('neighbors_giftinfo')}</div><div class="giftlist slick-scrollbar"></div></td>`;
 
     smartTable = new SmartTable(container.querySelector('.data'));
     smartTable.onSort = refresh;
@@ -50,6 +59,7 @@ function getState() {
     var getSort = (sortInfo, defaultValue) => sortInfo && (sortInfo.name != defaultValue || !sortInfo.ascending) ? smartTable.sortInfo2string(sortInfo) : '';
     return {
         show: selectShow.value,
+        gifts: selectDays.value,
         search: searchInput.value,
         sort: getSort(smartTable.sort, 'name')
     };
@@ -57,6 +67,15 @@ function getState() {
 
 function setState(state) {
     selectShow.value = state.show || '';
+    if (selectShow.selectedIndex < 0) {
+        selectShow.selectedIndex = 0;
+        state.show = selectShow.value;
+    }
+    selectDays.value = state.days || '';
+    if (selectDays.selectedIndex < 0) {
+        selectDays.selectedIndex = 21;
+        state.days = selectDays.value;
+    }
     searchInput.value = state.search || '';
     var sortInfo = smartTable.checkSortInfo(smartTable.string2sortInfo(state.sort), false);
     if (!sortInfo.name) {
@@ -65,14 +84,6 @@ function setState(state) {
     }
     smartTable.setSortInfo(sortInfo, false);
 }
-
-// function daysBetween(dt1, dt2) {
-//     if(dt1 > dt2) [dt1, dt2] = [dt2, dt1];
-//     dt1.setHours(4, 0, 0, 0);
-//     dt2.setHours(22, 0, 0, 0);
-//     var millis = dt2.getTime() - dt1.getTime();
-//     return Math.round(millis / 86400000);
-// }
 
 function onClick(e) {
     var cell;
@@ -86,27 +97,33 @@ function onClick(e) {
     }
     var giftContainer = trGifts.querySelector('.giftlist');
     giftContainer.innerHTML = '';
-    giftContainer.style.width = row.offsetWidth + 'px';
+    giftContainer.style.width = (row.offsetWidth - 2) + 'px';
     row.parentNode.insertBefore(trGifts, row.nextSibling);
     var id = row.getAttribute('data-pal-id');
     var pal = bgp.Data.getNeighbour(id);
-    var palGifts = pal.extra.gifts;
-    if (!Array.isArray(palGifts)) palGifts = [];
     var gifts = gui.getFile('gifts');
     var htm = '';
-    for (let palGift of palGifts) {
-        let gift = gifts[palGift.gid];
-        if (!gift) continue;
-        htm += HtmlBr `<div><img class="outlined" width="50" height="50" src="${gui.getObjectImage(gift.type, gift.object_id)}" title="${gui.getObjectName(gift.type, gift.object_id)}">`;
-        htm += HtmlBr `<b class="outlined">${Locale.formatNumber(+gift.amount)}</b>`;
-        htm += HtmlBr `<span>${Locale.formatDate(palGift.time)}<br>${Locale.formatTime(palGift.time)}</span></div>`;
+    for (let palGift of palGifts[pal.id]) {
+        let piece = giftCache[palGift.gid];
+        if (piece === undefined) {
+            let gift = gifts[palGift.gid];
+            piece = '';
+            if (gift) {
+                piece += HtmlBr `<div><img src="${gui.getObjectImage(gift.type, gift.object_id)}" title="${gui.getObjectName(gift.type, gift.object_id)}">`;
+                piece += HtmlBr `<i>${Locale.formatNumber(giftValues[gift.def_id])}</i><b>${Locale.formatNumber(+gift.amount)}</b>`;
+            }
+            giftCache[palGift.gid] = piece;
+        }
+        if (piece == '') continue;
+        htm += piece + HtmlBr `<span>${Locale.formatDayMonth(palGift.time)}<br>${Locale.formatTime(palGift.time)}</span></div>`;
     }
     giftContainer.innerHTML = htm;
 }
 
 function update() {
+    lastGiftDays = 0;
     palRows = {};
-    for(let pal of Object.values(bgp.Data.getNeighbours())) {
+    for (let pal of Object.values(bgp.Data.getNeighbours())) {
         let row = document.createElement('tr');
         row.setAttribute('data-pal-id', pal.id);
         row.setAttribute('height', 61);
@@ -115,6 +132,44 @@ function update() {
     }
     // Remove Mr.Bill
     delete palRows[1];
+    // Determine gift value
+    giftCache = {};
+    giftValues = {
+        system2: 1, // Energy
+        material1: 1, // Coin
+        material2: 1000, // Gem
+        material3: 60, // Copper
+        material6: 150, // Tin
+        material7: 30, // Lumber
+        material9: 75, // Coal
+        material11: 60, // Root
+        material19: 30, // Mushroom
+        material20: 20, // Apple
+        material21: 60, // Herb
+        material22: 50, // Stone
+        material29: 20, // Berry
+        material33: 190 // Iron ore
+    };
+    for (let gift of Object.values(gui.getFile('gifts'))) {
+        let type = gift.type;
+        let oid = gift.object_id;
+        let key = type + oid;
+        let value = giftValues[key];
+        if (value === undefined) {
+            value = 0;
+            if (type == 'system') {
+                value = 1;
+            } else if (type == 'usable') {
+                let o = bgp.Data.getObject(type, oid);
+                value = o ? +o.value : 0;
+            } else if (type == 'decoration' || type == 'windmill') {
+                let o = bgp.Data.getObject(type, oid);
+                value = o ? +o.sell_price : 0;
+            }
+            giftValues[key] = value;
+        }
+        giftValues[gift.def_id] = value * +gift.amount;
+    }
     refresh();
 }
 
@@ -125,7 +180,7 @@ function updateRow(row) {
     var htm = '';
     htm += HtmlBr `<td>${anchor}<img height="50" width="50" src="${gui.getFBFriendAvatarUrl(pal.fb_id)}"/></a></td>`;
     var friend = Object.values(bgp.Data.getFriends()).find(friend => friend.uid == id);
-    if(friend) {
+    if (friend) {
         htm += HtmlBr `<td>${gui.getFriendAnchor(friend)}<img class="friend" src="/img/gui/isaFriend.png"/></a>${anchor}${gui.getPlayerNameFull(pal)}</a></td>`;
     } else {
         htm += HtmlBr `<td>${anchor}${gui.getPlayerNameFull(pal)}</a></td>`;
@@ -148,9 +203,10 @@ function updateRow(row) {
         htm += HtmlBr `<td></td>`;
     }
     htm += HtmlBr `<td>${Locale.formatDate(pal.extra.timeCreated)}<br>${Locale.formatDays(pal.extra.timeCreated)}</td>`;
-    var gifts = pal.extra.gifts;
-    var count = Array.isArray(gifts) ? gifts.length : 0;
+    var gifts = palGifts[pal.id];
+    var count = gifts.length;
     htm += HtmlBr `<td class="${count > 0 ? 'has-gifts' : ''}">${Locale.formatNumber(count)}</td>`;
+    htm += HtmlBr `<td class="${count > 0 ? 'has-gifts' : ''}">${Locale.formatNumber(gifts._value)}</td>`;
     row.innerHTML = htm;
 }
 
@@ -167,6 +223,13 @@ function refresh() {
     scheduledRefresh = setTimeout(refreshDelayed, 50);
 }
 
+function getDateAgo(days) {
+    var dt = new Date();
+    dt.setHours(0, 0, 0, 0);
+    dt.setDate(dt.getDate() - days);
+    return dt.getTime() / 1000;
+}
+
 function refreshDelayed() {
     scheduledRefresh = 0;
     var state = getState();
@@ -176,20 +239,35 @@ function refreshDelayed() {
     if (show == 'inlist' || show == 'notinlist') {
         list = show == 'inlist' ? 0 : 1;
         show = 'list';
-    } else {
-        days = +show || 0;
+    } else if (show.startsWith('nogift')) {
+        days = +(show.substr(6)) || 0;
         show = days > 0 ? 'days' : '';
-        if (days) {
-            var dt = new Date();
-            dt.setHours(0, 0, 0, 0);
-            dt.setDate(dt.getDate() - days);
-            days = dt.getTime() / 1000;
-        }
+        if (days) days = getDateAgo(days);
     }
 
     var neighbors = Object.assign({}, bgp.Data.getNeighbours());
     delete neighbors[1];
     neighbors = Object.values(neighbors);
+
+    let giftDays = Math.min(7, +state.gifts || 0);
+    let giftThreshold = getDateAgo(giftDays - 1);
+    if (giftDays != lastGiftDays) {
+        lastGiftDays = giftDays;
+        palGifts = {};
+        for (let pal of neighbors) {
+            let gifts = Array.isArray(pal.extra.gifts) ? pal.extra.gifts : [];
+            let value = 0;
+            gifts = gifts.filter(gift => {
+                if (gift.time >= giftThreshold) {
+                    value += (giftValues[gift.gid] || 0);
+                    return true;
+                }
+            });
+            gifts._value = value;
+            palGifts[pal.id] = gifts;
+        }
+    }
+
     var rows = [];
     var getSortValueFunctions = {
         region: pal => +pal.region,
@@ -198,11 +276,12 @@ function refreshDelayed() {
         lastgift: pal => pal.extra.lastGift || 0,
         list: pal => +pal.c_list ? 0 : 1,
         recorded: pal => pal.extra.timeCreated || 0,
-        gifts: pal => Array.isArray(pal.extra.gifts) ? pal.extra.gifts.length : 0
+        gifts: pal => palGifts[pal.id].length,
+        value: pal => palGifts[pal.id]._value
     };
     var sortName = smartTable.sort.name;
-    var getSortValue = getSortValueFunctions[sortName];
-    for(let pal of neighbors) {
+    var getSortValue = getSortValueFunctions[sortName] || (_pal => 0);
+    for (let pal of neighbors) {
         if (show == 'list' && list != (+pal.c_list ? 0 : 1)) continue;
         else if (show == 'days' && (pal.extra.lastGift || pal.extra.timeCreated) >= days) continue;
         let fullname = gui.getPlayerNameFull(pal).toUpperCase();
