974762948ed9619aea8572d128f93a8bcd6cce72
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2019-09-25 21:14:30 +0200

    Fixes: option is always visible

--- gui/tabs/options/options.js
+++ gui/tabs/options/options.js
@@ -100,7 +100,7 @@ function init() {
     if (bgp.Data.generator) option('gameLanguage', SUBOPTION, gameLanguages);
     option('autoLogin');
     option('keepDebugging', WARNING);
-    if (gui.getPreference('fixes')) option('fixes', TEXT);
+    option('fixes', TEXT);
     endSection();
     beginSection('ingame');
     option('injectGame', CRITICAL);
