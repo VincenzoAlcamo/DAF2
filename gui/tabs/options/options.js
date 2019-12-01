b08eb8095d913b2350e93dac23eb58111a4bab7b
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2019-12-01 20:19:37 +0100

    Changed the help text for disableAltGuard option

--- gui/tabs/options/options.js
+++ gui/tabs/options/options.js
@@ -100,7 +100,7 @@ function init() {
     if (bgp.Data.generator) option('gameLanguage', SUBOPTION, gameLanguages);
     option('autoLogin');
     option('keepDebugging', WARNING);
-    option('disableAltGuard');
+    option('disableAltGuard', WARNING);
     if (gui.getPreference('fixes')) option('fixes', TEXT);
     endSection();
     beginSection('ingame');
