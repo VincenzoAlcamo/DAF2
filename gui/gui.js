1b9b860b859298189f781429b5342b51059043eb
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2021-10-30 12:46:39 +0200

    Events: fix ending date

--- gui/gui.js
+++ gui/gui.js
@@ -232,7 +232,7 @@ const gui = {
             }
             // Fix bug in Halloween 2019 end date
             const start = (event && +event.start) || 0;
-            if (start > 0 && (end - start) / 86400 > 28) return start + 14 * 86400;
+            if (start > 0 && (end - start) / 86400 > 36) return start + 14 * 86400;
             return end;
         })();
         // compute the year as END - 14 days
