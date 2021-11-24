e419cda8bb670dfb07175ee1a1b4dffbaed350bb
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2021-11-24 12:15:00 +0100

    Equipment: show price combo if there is at least one valid price

--- gui/tabs/equipment/equipment.js
+++ gui/tabs/equipment/equipment.js
@@ -1088,7 +1088,7 @@ function showOffer(type, id, options) {
 			htm += Html.br`</select></td>`;
 		}
 		const prices = getDistincts(getSelection(Object.assign({}, current, { price: -1 })).map(block => block.price));
-		if (prices.length > 0) {
+		if (prices.length > 1 || (prices.length == 0 && prices[0] > 0)) {
 			htm += Html.br`<td>${gui.getMessage(type == 'tier' ? 'gui_type' : 'gui_cost')} <select name="price" data-method="price" style="margin-bottom:2px">`;
 			if (prices.length > 1) htm += optionHtml(-1, '[ ' + gui.getMessage('gui_all').toUpperCase() + ' ]', current.price);
 			for (const price of prices) htm += optionHtml(price, blocks.find(block => block.price == price).priceText, current.price);
