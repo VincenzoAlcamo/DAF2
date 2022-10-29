ff9fb552f63bf6d2275031b651df7d761d7c4087
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2022-10-29 10:23:45 +0200

    Import/Export: removed export diagnostic data Import/Export: moved export inventory to Progress tab

--- gui/tabs/export/export.js
+++ gui/tabs/export/export.js
@@ -5,59 +5,14 @@ export default {
 	update: update,
 	requires: ['materials', 'decorations', 'usables', 'windmills', 'buildings', 'tokens'],
 	events: {
-		inventory: exportInventory,
 		export: exportData,
-		import: importData,
-		debug: exportDebug
+		import: importData
 	}
 };
 
 function init() { }
 
 function update() { }
-
-function exportInventory() {
-	const dp = gui.getDateParts();
-	const filename = `DAF_inventory_${dp.date}_${dp.time.replace(/:/g, '')}.csv`;
-	let data = [];
-	const generator = gui.getGenerator();
-	data.push('LEVEL\t' + generator.level);
-	data.push('REGION\t' + generator.region);
-	data.push('DATE\t' + dp.date + ' ' + dp.time);
-	data.push('XP\t' + generator.exp);
-	data.push('CARAVANS\t' + generator.caravans.length);
-	data.push('POTS\t' + generator.pots.length);
-	data.push('ANVILS\t' + (generator.anvils ? generator.anvils.length : 1));
-	data.push('');
-	data.push('MAT_ID\tMAT_NAME\tQTY');
-	const addRow = (key, name, qty) => {
-		if (+qty > 0) data.push(key + '\t' + name + '\t' + qty);
-	};
-	const addList = (list, collection, keyPrefix) => {
-		Object.keys(list).forEach(key => {
-			const obj = collection[key];
-			if (obj && (keyPrefix != 'T' || +obj.visibility)) {
-				let name = gui.getString(obj.name_loc);
-				if (keyPrefix == 'U' && obj.action == 'add_stamina') name += ' (+' + +obj.value + ')';
-				addRow(keyPrefix + key, name, list[key]);
-			}
-		});
-	};
-	addList(generator.materials, gui.getFile('materials'), '');
-	addList(generator.tokens, gui.getFile('tokens'), 'T');
-	addList(generator.stored_windmills, gui.getFile('windmills'), 'W');
-	addList(gui.getOwnedActive('decoration').owned, gui.getFile('decorations'), 'D');
-	addList(gui.getOwnedActive('building').owned, gui.getFile('buildings'), 'B');
-	addList(generator.usables, gui.getFile('usables'), 'U');
-
-	data = data.join('\n');
-	gui.downloadData({ data, filename });
-}
-
-function exportDebug() {
-	const data = gui.getGenerator();
-	gui.downloadData({ data, filename: 'DAF_diagnostics_<date>_<time>.json' });
-}
 
 function exportData() {
 	const data = {};
