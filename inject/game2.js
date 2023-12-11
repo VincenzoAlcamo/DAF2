00d5a0e8cc6eac36cdd3aecce057b5e6f94192a4
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2023-12-11 19:23:31 +0100

    Merged game0.js and game2.js

--- inject/game2.js
+++ inject/game2.js
@@ -243,6 +243,28 @@ XHR.send = function() {
 	});
 }
 
+function interceptGame() {
+	const code = `
+window.$hxClasses = window.$hxClasses || {};
+const _ObjectCreate = Object.create;
+Object.create = function (proto) {
+	const obj = _ObjectCreate.apply(Object, arguments);
+	let __class__;
+	if (proto) Object.defineProperty(obj, '__class__', {
+		get() { return __class__; },
+		set(newValue) {
+			if (newValue && typeof newValue.__name__ == 'string') window.$hxClasses[newValue.__name__] = newValue;
+			__class__ = newValue;
+		},
+		enumerable: true,
+		configurable: true,
+	});
+	return obj;
+};
+`;
+	document.documentElement.appendChild(createScript(code));
+}
+
 function init() {
 	miner = document.getElementById('miner') || document.getElementById('canvas');
 	// Set body height to 100% so we can use height:100% in miner
@@ -679,4 +701,5 @@ if (extras.length) postMessage({ action: "sendValue", name: '@extra', value: ext
 	});
 }
 
-init();
+interceptGame();
+window.addEventListener('DOMContentLoaded', init);
