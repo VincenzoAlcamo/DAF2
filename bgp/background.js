96055ec76a54a2b5d36a95398cb1b2933241ec76
Vincenzo Alcamo <vincenzo.alcamo@gmail.com>
2024-01-24 12:55:08 +0100

    Remove guard in friend collection

--- bgp/background.js
+++ bgp/background.js
@@ -1197,8 +1197,6 @@ var Data = {
 		const oldFriendsById = Object.assign({}, Data.getFriends());
 		const oldFriendsByUri = {};
 		const numBefore = Object.values(oldFriendsById).length;
-		// We allow at most a loss of 5% of the current friends (or 10 if the player has less than 200 friends)
-		const maxRemove = Math.max(10, Math.floor(numBefore * 0.05));
 		Object.values(oldFriendsById).forEach(friend => oldFriendsByUri[friend.uri] = friend);
 		const friends = {};
 		const now = getUnixTime();
@@ -1230,7 +1228,7 @@ var Data = {
 			delete oldFriendsByUri[friend.uri];
 			friends[friend.id] = friend;
 		}
-		if (partial || Object.values(oldFriendsById).length > maxRemove) {
+		if (partial) {
 			Data.saveFriend(Object.values(friends));
 			Object.entries(oldFriendsById).forEach(([id, friend]) => friends[id] = friend);
 		} else {
